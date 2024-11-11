9112830e243fb8e3e3d2c44f6c938f2902b2cb19
HP <HP@DESKTOP-4G77UKH>
2024-11-11 10:42:07 +0530

    Add project files.

---
+++ Count Vowels/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string :");
+            string str = Console.ReadLine().ToLower();
+            int count = 0;
+            foreach(int c in str)
+            {
+                if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+                {
+                    count++;
+                }
+            }
+            if(count>0)
+            {
+                Console.WriteLine("No of vowels " + count);
+            }
+            else
+            {
+                Console.WriteLine("no vowels ");
+            }
+        }
+    }
+}
---
+++ Count Vowels/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Count Vowels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Count Vowels")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1692cb40-b633-4a24-86c7-c47bc508a5d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
